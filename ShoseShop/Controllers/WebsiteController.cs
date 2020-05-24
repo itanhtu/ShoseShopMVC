42171bb085f3c4cccbd688e083245287385c00c0
itanhtu <anhtu01679353361@gmail.com>
2020-05-24 21:22:27 +0700

    Add LayoutWeb

---
+++ ShoseShop/Controllers/WebsiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoseShop.Controllers
+{
+    public class WebsiteController : Controller
+    {
+        // GET: Website
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
